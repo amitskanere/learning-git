9181d61168de7cecd12e94b7dfd1f8ed21930010
amit kanere <amitkanere1@gmail.com>
2021-10-21 14:26:09 +0530

    commit for uploading new files after a long time

--- new_ex.py
+++ new_ex.py
@@ -1,32 +1,8 @@
-def mergesort(unsortedlist):
-     if len(unsortedlist) <=1:
-          return unsortedlist
-     middle = len(unsortedlist)//2
-     left_list = unsortedlist[:middle]
-     right_list = unsortedlist[middle:]
+N = int(input())
+lst = []
+for value in range(N):
+     i,e = int(input('insert')).split()
+     lst.insert(i,e)
+print(lst)
 
-     left_list = mergesort(left_list)
-     right_list = mergesort(right_list)
-     return list(merge(left_list,right_list))
-def merge(left_half,right_half):
-     res =[]
-     while len(left_half) != 0 and len(right_half) != 0:
-          if left_half[0] < right_half[0]:
-               res.append(left_half[0])
-               left_half.remove(left_half[0])
-          else:
-               res.append(right_half[0])
-               right_half.remove(right_half[0])
-     if len(left_half) == 0:
-          res = res + right_half
-     else:
-          res = res + left_half
-
-     return res
-
-unsortedlist = [23,45,2,98,56,83,67,29,74]     
-print(mergesort(unsortedlist))
-
-     
-
-     
+             
